0ef421c5ab349c5b77c9079269a25d94b8e109cc
Carlos Anderson <carlosjanderson@gmail.com>
2014-07-15 18:06:20 -0400

    Add initial bamm.rb

---
+++ bamm.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class Bamm < Formula
+  homepage "http://bamm-project.org"
+  url "https://github.com/macroevolution/bamm.git", :tag => "v2.0.0"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
